fc926ab75374036c9cabafea95d7b11aa743721f
Eldar Shahmaliyev <shahmal1yev@mail.ru>
2024-09-07 19:06:15 +0400

    Update ResourceContract interface with asset methods
    - Added two new methods to the ResourceContract interface:
      - `get(string $name)` to retrieve an asset by name, throwing a `BadAssetCallException` if the asset does not exist.
      - `exist(string $name): bool` to check if an asset exists on the resource.

--- src/Contracts/HTTP/Resources/ResourceContract.php
+++ src/Contracts/HTTP/Resources/ResourceContract.php
@@ -5,6 +5,7 @@ namespace Atproto\Contracts\HTTP\Resources;
 use Atproto\API\App\Bsky\Actor\GetProfile;
 use Atproto\API\Com\Atrproto\Repo\CreateRecord;
 use Atproto\API\Com\Atrproto\Repo\UploadBlob;
+use Atproto\Exceptions\Resource\BadAssetCallException;
 
 /**
  * @see GetProfile
@@ -13,4 +14,17 @@ use Atproto\API\Com\Atrproto\Repo\UploadBlob;
  */
 interface ResourceContract
 {
+    /**
+     * @param  string  $name
+     * @return mixed
+     *
+     * @throws BadAssetCallException If the asset does not exist on resource.
+     */
+    public function get(string $name);
+
+    /**
+     * @param  string  $name
+     * @return bool
+     */
+    public function exist(string $name): bool;
 }
