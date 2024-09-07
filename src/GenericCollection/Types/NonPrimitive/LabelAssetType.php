21f07cf43519f261e0b59c76b5d3a8edf84d594f
Eldar Shahmaliyev <shahmal1yev@mail.ru>
2024-09-07 19:07:08 +0400

    Add new non-primitive types: FollowerAssetType and LabelAssetType
    - Created two new classes: `FollowerAssetType` and `LabelAssetType` under `Atproto\GenericCollection\Types\NonPrimitive`.
    - Both classes implement the `TypeInterface` and define a `validate` method that checks if a given value is an instance of `FollowerAsset` or `LabelAsset` respectively.

---
+++ src/GenericCollection/Types/NonPrimitive/LabelAssetType.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Atproto\GenericCollection\Types\NonPrimitive;
+
+use Atproto\Resources\Assets\LabelAsset;
+use GenericCollection\Interfaces\TypeInterface;
+
+class LabelAssetType implements TypeInterface
+{
+
+    public function validate($value): bool
+    {
+        return $value instanceof LabelAsset;
+    }
+}
