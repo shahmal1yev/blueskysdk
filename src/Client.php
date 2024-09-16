533f64ebfa3038586a19716a06d599d325a1a56c
Eldar Shahmaliyev <shahmal1yev@mail.ru>
2024-09-16 20:53:52 +0400

    refactor: Extract Client logic into Smith trait

--- src/Client.php
+++ src/Client.php
@@ -2,54 +2,9 @@
 
 namespace Atproto;
 
-use Atproto\Contracts\RequestContract;
-use Atproto\Exceptions\Http\Request\RequestNotFoundException;
-use Exception;
+use Atproto\Traits\Smith;
 
 class Client
 {
-    protected array $path = [];
-
-    public function __call(string $name, array $arguments): Client
-    {
-        $this->path[] = $name;
-
-        return $this;
-    }
-
-    protected function refresh(): void
-    {
-        $this->path = [];
-    }
-
-    /**
-     * @throws RequestNotFoundException
-     */
-    public function build(): RequestContract
-    {
-        $namespace = $this->namespace();
-
-        if (! class_exists($namespace)) {
-            throw new RequestNotFoundException("$namespace class does not exist.");
-        }
-
-        $request = new $namespace;
-
-        $request->build();
-
-        return $request;
-    }
-
-    protected function namespace(): string
-    {
-        $prefix = '\\Atproto\\HTTP\\API\\Requests\\';
-        $namespace = "$prefix" . implode('\\', array_map(
-                'ucfirst',
-                $this->path
-            ));
-
-        $this->refresh();
-
-        return $namespace;
-    }
+    use Smith;
 }
---
+++ src/Traits/Smith.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Atproto\Traits;
+
+use Atproto\Client;
+use Atproto\Contracts\RequestContract;
+use Atproto\Exceptions\Http\Request\RequestNotFoundException;
+
+trait Smith
+{
+    protected array $path = [];
+    protected static string $prefix = "\\Atproto\\HTTP\\API\\Requests\\";
+
+    public function __call(string $name, array $arguments): Client
+    {
+        $this->path[] = $name;
+
+        return $this;
+    }
+
+    protected function refresh(): void
+    {
+        $this->path = [];
+    }
+
+    /**
+     * @throws RequestNotFoundException
+     */
+    public function forge(): RequestContract
+    {
+        $namespace = $this->namespace();
+
+        if (! class_exists($namespace)) {
+            throw new RequestNotFoundException("$namespace class does not exist.");
+        }
+
+        return new $namespace;
+    }
+
+    protected function namespace(): string
+    {
+        $namespace = self::$prefix . implode('\\', array_map(
+                'ucfirst',
+                $this->path
+            ));
+
+        $this->refresh();
+
+        return $namespace;
+    }
+}
