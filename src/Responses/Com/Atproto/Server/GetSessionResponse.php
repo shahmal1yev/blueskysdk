1eebafbaebbb7726d654dbd00d6ddc996c10058e
Eldar Shahmaliyev <shahmal1yevv@proton.me>
2025-01-25 23:12:57 +0400

    feat: implement GetSession endpoint and response

---
+++ src/Responses/Com/Atproto/Server/GetSessionResponse.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Atproto\Responses\Com\Atproto\Server;
+
+use Atproto\Contracts\Resources\ResponseContract;
+use Atproto\Responses\BaseResponse;
+
+class GetSessionResponse implements ResponseContract
+{
+    use BaseResponse;
+}
