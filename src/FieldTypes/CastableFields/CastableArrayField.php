884ec516973520e921f53cacfba97fe4dc190b50
Eldar Shahmaliyev <shahmal1yevv@proton.me>
2025-03-27 00:34:04 +0400

    Add castable field classes for array, object, and union types

---
+++ src/FieldTypes/CastableFields/CastableArrayField.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Atproto\FieldTypes\CastableFields;
+
+/**
+ * @method self|null|int maxLength(?int $maxLength = null)
+ * @method self|null|int minLength(?int $minLength = null)
+ * @method self|null|string items(?string $items = null)
+ * @method self|null|string description(?string $description = null)
+ */
+class CastableArrayField extends CastableField
+{
+}
